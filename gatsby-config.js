d836899bb2bec22f83764b42a6770cb661a482b6
Musuauergit config --global user.email musuauer@gmail.com <info@guillermogudino.com>
2019-01-22 21:39:49 +0100

    configure contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,13 @@ module.exports = {
     title: 'Carsten Horn'
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CF_SPACE || '8jzh9bya95xd',
+        accessToken: process.env.CF_TOKEN || '50dfc5b584292518e2d1956b5173c544e54993e909b1fda82b0d92f5c528ed71'
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,6 +31,7 @@ module.exports = {
         path: `${__dirname}/static/img/homepage`
       }
     },
+    'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
