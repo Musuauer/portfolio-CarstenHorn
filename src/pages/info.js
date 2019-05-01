b4b4d8a0d5d86b2d1ae12fd1d00e7a648ceec33c
Musuauergit config --global user.email musuauer@gmail.com <info@guillermogudino.com>
2019-05-01 07:49:58 +0200

    feat: add shows

--- src/pages/info.js
+++ src/pages/info.js
@@ -14,6 +14,74 @@ const Info = (props) => (
                 bio
               }
               email
+              shows{
+                _1{
+                  date
+                  text
+                  place
+                  title
+                }
+                _2{
+                  date
+                  text
+                  place
+                  title
+                }
+                _3{
+                  date
+                  text
+                  place
+                  title
+                }
+                _4{
+                  date
+                  text
+                  place
+                  title
+                }
+                _5{
+                  date
+                  text
+                  place
+                  title
+                }
+                _6{
+                  date
+                  text
+                  place
+                  title
+                }
+                _7{
+                  date
+                  text
+                  place
+                  title
+                }
+                _8{
+                  date
+                  text
+                  place
+                  title
+                }
+                _9{
+                  date
+                  text
+                  place
+                  title
+                }  
+                _10{
+                  date
+                  text
+                  place
+                  title
+                }  
+                _11{
+                  date
+                  text
+                  place
+                  title
+                }
+              }
               pdf{
                 title
                 file{
@@ -32,11 +100,36 @@ const Info = (props) => (
           <div className='info-text'>
 
             {console.log('info', data.allContentfulInfo.edges[0].node)}
-            <h2>E-Mail: <a href={`mailto: ${data.allContentfulInfo.edges[0].node.email}`} target='_top'>{data.allContentfulInfo.edges[0].node.email}</a></h2>
+
+            <h2>E-Mail: <a href={`mailto: ${data.allContentfulInfo.edges[0].node.email}`} target='_top'>
+
+              {data.allContentfulInfo.edges[0].node.email}</a></h2>
+
             {data.allContentfulInfo.edges[0].node.bio.bio.split('\n').map((line, index) =>
               <p key={index}>
                 {line}
               </p>)}
+
+            <div className='shows'>
+              <h3>Austellungen</h3>
+              {console.log('shows...', Object.entries(data.allContentfulInfo.edges[0].node.shows).sort(([, v1], [, v2]) => +v2 - +v1))}
+              {Object.entries(data.allContentfulInfo.edges[0].node.shows).map(show =>
+                <div className='show' key={show[0]}>
+                  <p>
+                    <span>
+                      {show[1].date} {show[1].title}
+                    </span>
+                  </p>
+                  <p>
+                    {show[1].text} {show[1].place}
+
+                  </p>
+
+                </div>
+              ).sort(function (a, b) {
+                return a < b ? -1 : (a > b ? 1 : 0)
+              })}
+            </div>
 
           </div>
         </div>
